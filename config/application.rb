b8452f215caec6361afd252afdb7174850f215ec
居谷和彦 <itanikazuhiko@itanikazuhikonoMacBook-Pro.local>
2018-05-10 20:39:50 +0900

    finish rspec setup

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,10 @@ module BlogLog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.test_framework :rspec,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
